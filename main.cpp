7191080e395e62a8e489fe479877912c209bb813
Sholi1 <hitotsuchance@gmail.com>
2026-09-12 13:27:51 -0500

    added user interface in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,26 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+  int choice=9;
+
+  while (choice!=9) {
+    cout<<"===== Campus Resource Reservation System =====\n\n
+      1. View Resources\n
+      2. Create Reservation\n
+      3. Cancel Reservation\n
+      4. View Waiting Lists\n
+      5. Undo Cancellation\n
+      6. Search Reservations\n
+      7. Sort Resources\n
+      8. Generate Report\n
+      9. Exit\n\n
+      Enter Choice: ";
+
+    
+  }
+
+
+
+  return 0;
+}
