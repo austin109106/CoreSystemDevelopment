02b5c24a091df39f161911d129dc14dd85a8624f
austin109106 <167753311+austin109106@users.noreply.github.com>
2026-09-16 01:34:15 +0000

    Pending changes exported from your codespace

---
+++ Reservations.h
@@ -0,0 +1,49 @@
+#pragma once
+#ifndef RESERVATIONS_H
+#define RESERVATIONS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Reservations {
+    private:
+        //variables
+        int resNumber, studentID, resourceNumber, day, month, year;
+        string name;
+
+    // |res number |student number |name |resource number |date |
+    public:
+        //constructors
+        Reservations();
+        Reservations(int rNumber, int SID, string name, int recNumber, int d, int m, int y);
+
+        //setters
+        void setReservationNumber(int rNumber);
+        void setStudentID(int SID);
+        void setName(string name);
+        void setResourceNumber(int rNum);
+        void setDate(int d, int m, int y);
+
+        //getters
+        int getReservationNumber();
+        int getStudentID();
+        string getName();
+        int getResourceNumber();
+        string getDate(); //returns 09/15/2026
+
+
+        //operator overloads
+        bool operator==(const Reservations& other) const;
+
+        bool operator>(const Reservations& other) const;
+        bool operator<(const Reservations& other) const;
+
+        //print
+        void display();
+
+};
+
+
+#endif //RESERVATIONS_H
