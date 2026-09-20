42621f2492236b54c5ec74b28b3b8e16963afa49
austin109106 <167753311+austin109106@users.noreply.github.com>
2026-09-20 17:14:54 +0000

    Pending changes exported from your codespace

--- ReservationsList.h
+++ ReservationsList.h
@@ -3,7 +3,6 @@
 #define RESERVATIONSLIST_H
 
 #include "Reservations.h"
-#include <fstream>
 
 class ReservationsList {
 private:
@@ -11,20 +10,27 @@ private:
         Reservations data;
         Node* next;
 
-        Node (int rNum, int sNum, string name, int recNum, int d, int m, int y) : data(rNum, sNum, name, recNum, d, m, y) , next(nullptr);
+        Node (const Reservations& r) : data(r), next(nullptr) {}
     };
 
     Node* head;
     Node* tail;
+    int count;
 
 public:
     ReservationsList();
     ~ReservationsList();    //dynamic value needs to be deleted
 
-    void append(int rNum, int sNum, string name, int recNum, int d, int m, int y); 
+    ReservationsList(const ReservationsList) = delete; // ensures no doubleing
+    ReservationsList& operator=(const ReservationsList&) = delete;
+    
+    
+    void add(const Reservations& r); 
     bool loadFromFile(const string& filename);
     void display() const;
     void printEarliest() const;
+    int size() const;
+    void clear();
 
 };
 
--- Reservatons.cpp
+++ Reservatons.cpp
@@ -1,4 +1,6 @@
 #include "Reservations.h"
+#include <sstream>
+#include<iomanip>
 
 Reservations::Reservations(){
     resNumber = 0;
@@ -10,11 +12,11 @@ Reservations::Reservations(){
     year = 2026;
 };
 
-Reservations::Reservations(int rNumber, int SID, string name, int resNumber, int d, int m, int y){
+Reservations::Reservations(int rNumber, int SID, string name, int resourceNum, int d, int m, int y){
     resNumber = rNumber;
     studentID = SID;
     this->name = name;
-    resourceNumber = resNumber;
+    resourceNumber = resourceNum;
     day = d;
     month = m;
     year = y;
@@ -33,14 +35,18 @@ void Reservations::setDate(int d, int m, int y){
 }
 
 //getters
-int Reservations::getReservationNumber(){ return resNumber; }
-int Reservations::getStudentID(){ return studentID; }
-string Reservations::getName(){ return name;}
-int Reservations::getResourceNumber(){ return resourceNumber; }
+int Reservations::getReservationNumber() const { return resNumber; }
+int Reservations::getStudentID() const { return studentID; }
+string Reservations::getName() const { return name;}
+int Reservations::getResourceNumber() const { return resourceNumber; }
+int Reservations::getDay() const { return day; }
+int Reservations::getMonth() const { return month; }
+int Reservations::getYear() const { return year; }
 
 string Reservations::getDate(){
-    string date = to_string(day) + '/' + to_string(month) + '/' + to_string(year);
-    return date;
+    ostringstream oss;
+    oss << setfill('0') << setw(2) << month << '/' << setw(2) << day << '/' << year;
+    return oss.str();
 } //returns 09/15/2026
 
 
@@ -52,12 +58,55 @@ bool Reservations::operator>(const Reservations& other) const{
     return this->resNumber > other.resNumber;
 }
 
-bool Reservations::operator>(const Reservations& other) const{
+bool Reservations::operator<(const Reservations& other) const{
     return this->resNumber < other.resNumber;
 }
 
+ostream& operator<<(ostream& out, const Reservations& r){
+    out << r.resNumber << "|" << r.studentID << "|" << r.name << "|R" << r.resourceNumber << "|" << setfill('0') << setw(2) << r.month << "/" << setw(2) << r.day << "/" << r.year << setfill(' ');
+//setw(2)/setfill('0') is for single digit days of the month, I.E "09/21/2001" instead of "9/21/2001"
+    return out;
+}
+
+istream& operator>>(istream& in, Reservations r){
+    string res;
+    if(!getline(in, line)){
+        return in;
+    }
+    if(line.empty()){
+        in.setstate(ios::failbit);
+        return in;
+    }//error handling
+
+    stringstream ss(line);
+    string rStr, sidStr, resStr, dateStr;
+
+    getline(ss, rStr, '|');
+    getline(ss, sidStr, '|');
+    getline(ss, r.name, '|');
+    getline(ss, resStr, '|');
+    getline(ss, dateStr);
+    //disects reservation
+
+    r.resNumber = stoi(rStr); //stoi = string to int
+    r.studentID = stoi (sidStr);
+    r.resourceNumber = stoi(resStr.substr(1));//grebs resNumber without the R
+    
+    stringstream ds(dateStr);
+    string monthStr, dayStr, yearStr;
+    getline(ds, monthStr, '/');
+    getline(ds, dayStr, '/');
+    getline(ds, yearStr);
+
+    r.month = stoi(monthStr);
+    r.day = stoi(dayStr);
+    r.year = stoi(yearStr);
+
+    return in;
+}
+
+
 void Reservations::display() const {
-    string d  = this->getDate();
-    cout << "|" << resNumber << "|" << studentID << "|" << name << "|R" << resourceNumber << "|" << d << "|";
+    cout << "|" << resNumber << "|" << studentID << "|" << name << "|R" << resourceNumber << "|" << getDate() << "|";
 }
 
