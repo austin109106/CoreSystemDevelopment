eeb9e13620ce0f9b44e1a8b3585285c8a709008c
payton10230 <hitotsuchance@gmail.com>
2026-09-20 21:51:44 -0500

    Create WaitingList.h
    yey

---
+++ WaitingList.h
@@ -0,0 +1,34 @@
+//waitlist class for specfic function when choice==2,choice==3,choice==4,and choice==5
+#ifndef WAITING_LIST
+#define WAITING_LIST
+
+#include <queue>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class WaitingList{
+    public:
+        WaitingList();
+        
+        struct Entry {
+            int studentID;
+            string name;
+            int resourceID;
+        };
+        
+        void displayWL() const;
+        
+        void addtoWL(int SID, string n, int RID);
+        
+        Entry removefromWL(int resourceID);
+        
+        bool isEmpty(int resourceID);
+        
+    private:
+        //queue for waiting list
+        map<int, queue<Entry>> waitinglist;
+        
+    
+};
